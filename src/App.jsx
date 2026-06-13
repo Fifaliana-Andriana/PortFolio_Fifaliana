f2eea03eabb4544ca688bb56d767c221f4030814
Fifaliana Andriana <andrianjafyfifaliana@gmail.com>
2026-06-13 12:32:44 +0300

    Les compétences

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./composant/Header";
 import Outlet from "./component/Outlet";
 import Footer from "./component/Footer";
 import { FaCircleArrowUp } from "react-icons/fa6";
 
 function App() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <div>
       <div className="posFix z-100 right0">
-          <a href="#Home" className={window.Header ? "opacity0" : "opacity1"}><FaCircleArrowUp/></a>
+        <a href="#Home" className={show ? "opacity1" : "opacity0"}>
+          <FaCircleArrowUp />
+        </a>
       </div>
       <div>
         <Header />
--- src/composant/Competence.jsx
+++ src/composant/Competence.jsx
@@ -9,58 +9,99 @@ import { FaReact } from "react-icons/fa6";
 import { FaPhp } from "react-icons/fa6";
 import { FaLaravel } from "react-icons/fa6";
 import { FaLinux } from "react-icons/fa6";
+import { FaCode } from "react-icons/fa6";
+// import { SiVisualstudiocode } from "react-icons/si";
 
 function Competence() {
   return (
-    <div id="Competence" className="w100prc bgBlue">
+    <div id="Competence" className="w100prc container">
       <h2>Compétences Techniques</h2>
-      <div className="skills displayF flexDirC">
-        <div className="displayF justifyCSE mBottom">
-          <div className="skill-card displayF justifyCSE alignIC w5cm">
-            <FaHtml5 />
-            HTML
+      <div className="containerFirst">
+        <div className="containerSecond">
+          <div className="containerThird">
+            <p className="h15cm">Développement Front-end</p>
+            <div className="skill-card displayF justifyCC alignIC w5cm">
+              <i>
+                <FaHtml5 />
+              </i>
+              <p>HTML</p>
+            </div>
+            <div className="skill-card displayF justifyCC alignIC w5cm">
+              <i>
+                <FaCss3 />
+              </i>
+              <p>CSS</p>
+            </div>
+            <div className="skill-card displayF justifyCSE alignIC w5cm">
+              <i>
+                <FaJs />
+              </i>
+              <p>JavaScript</p>
+            </div>
+            <div className="skill-card displayF justifyCSE alignIC w5cm">
+              <i>
+                <FaReact />
+              </i>
+              <p>React</p>
+            </div>
+            <div className="skill-card displayF justifyCSE alignIC w5cm">
+              <i>TW</i>
+              <p>Tailwind CSS</p>
+            </div>
+            <div className="skill-card displayF justifyCSE alignIC w5cm">
+              <i>
+                <FaBootstrap />
+              </i>
+              <p>Bootstrap</p>
+            </div>
           </div>
-          <div className="skill-card displayF justifyCSE alignIC w5cm">
-            <FaCss3 />
-            CSS
-          </div>
-          <div className="skill-card displayF justifyCSE alignIC w5cm">
-            <FaJs />
-            JavaScript
-          </div>
-          <div className="skill-card displayF justifyCSE alignIC w5cm">
-            <FaReact />
-            React
+          <div className="containerThird">
+            <p className="h15cm">Développement Back-end</p>
+            <div className="skill-card displayF justifyCSE alignIC w5cm">
+              <i>
+                <FaPhp />
+              </i>
+              <p>PHP 8+</p>
+            </div>
+            <div className="skill-card displayF justifyCSE alignIC w5cm">
+              <i>
+                <FaLaravel />
+              </i>
+              <p>Laravel</p>
+            </div>
           </div>
         </div>
-        <div className="displayF justifyCSE mBottom">
-          <div className="skill-card displayF justifyCSE alignIC w5cm">
-            <FaPhp />
-            PHP 8+
+        <div className="containerSecond">
+          <div className="containerThird">
+            <p className="h15cm">Bases de données</p>
+            <div className="skill-card displayF justifyCSE alignIC w5cm">
+              <i>
+                <FaMdb />
+              </i>
+              <p>MySQL</p>
+            </div>
           </div>
-          <div className="skill-card displayF justifyCSE alignIC w5cm">
-            <FaLaravel />
-            Laravel
-          </div>
-          <div className="skill-card displayF justifyCSE alignIC w5cm">
-            <FaMdb />
-            MySQL
-          </div>
-
-          <div className="skill-card displayF justifyCSE alignIC w5cm">Tailwind CSS</div>
-        </div>
-        <div className="displayF justifyCSE mBottom">
-          <div className="skill-card displayF justifyCSE alignIC w5cm">
-            <FaBootstrap />
-            Bootstrap
-          </div>
-          <div className="skill-card displayF justifyCSE alignIC w5cm">
-            <FaGithub />
-            Git & GitHub
-          </div>
-          <div className="skill-card displayF justifyCSE alignIC w5cm">
-            <FaLinux />
-            Linux Server
+          <div className="containerThird">
+            <p className="h15cm">Outils & Environnement</p>
+            <div className="skill-card displayF justifyCSE alignIC w5cm">
+              <i>
+                <FaGithub />
+              </i>
+              <p>Git & GitHub</p>
+            </div>
+            <div className="skill-card displayF justifyCSE alignIC w5cm">
+              {/* <SiVisualstudiocode/> */}
+              <i>
+                <FaCode />
+              </i>
+              <p>Visual Studio Code</p>
+            </div>
+            <div className="skill-card displayF justifyCSE alignIC w5cm">
+              <i>
+                <FaLinux />
+              </i>
+              <p>Linux Server</p>
+            </div>
           </div>
         </div>
       </div>

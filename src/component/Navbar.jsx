c7d7b4ad3317ed9799de1c4b94cf742d47017db6
Fifaliana Andriana <andrianjafyfifaliana@gmail.com>
2026-06-22 13:40:33 +0300

    Le nom

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,10 +5,10 @@ function Navbar() {
   return (
     <nav className="displayF justifyCSB alignIC posFix z-100 w100prc">
       <div className="w25prc">
-        <h1>Logo</h1>
+        <h1 className="colorWhite">Logo</h1>
       </div>
       <div className="w70prc">
-        <div className="w100prc">
+        <div className="w100prc colorWhite">
           <Respstyle></Respstyle>
         </div>
       </div>
